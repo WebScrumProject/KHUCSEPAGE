68cbdde2cd7c825a210a3900635f2752c4fca267
Hadan <zinzzaboy00@gmail.com>
2024-02-12 17:19:43 +0900

    24.02.12 add Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,19 +9,14 @@ import { Route } from 'react-router';
 import Detail from './pages/Detail';
 import P_List_add_design from './pages/P_List_add_design';
 import P_calendar from './components/calendar';
+import Routing from './routes/Routes';
 
 function App() {
   const scholarshipId = 0;
   const [value, onChange] = useState(new Date());
   return (
     
-      <Routes>
-        <Route path="/" element={<Mynavbar/>}></Route>
-        <Route path="/calendar" element={<P_calendar onChange={onChange} value={value}></P_calendar>}></Route>
-        <Route path="/scholarship" element={<J_List/>}></Route>
-        <Route path="/scholarship/:key/:page" element={<Detail />} />
-        <Route path="/project/write" element={<P_List_add_design/>}/>
-      </Routes>
+      <Routing></Routing>
     
   );
 }
